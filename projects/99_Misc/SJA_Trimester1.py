2a83ac404347441ff0f457ea7fd3ec151ad80f39
JedGStumpf <jedgstumpf@gmail.com>
2024-11-10 18:40:53 +0000

    Trimester 1 SJA knowlege test

---
+++ projects/99_Misc/SJA_Trimester1.py
@@ -0,0 +1,44 @@
+"""
+On your own follow the below instructions to show your code knowlege at the end of Trimester 1
+"""
+
+
+# 1.  Create 3 variables, 1 each of String, Int, and Boolean
+
+
+
+
+
+# 2.  Write a for loop that prints the numbers 1-10
+
+
+
+
+# 3.  Write a for loop that prints each letter of the string you created in the 1st task
+
+
+
+
+# 4.a  Create a function that takes in 2 arguments, both integers and returns the sum of those 2 arguments
+
+
+
+
+# 4.b. Call the function from task 4.a
+
+
+
+# EXTRA CREDIT !!!
+# EC_1.a  Create a function that takes in a string as an argument and returns a list containing each letter of the string
+
+
+
+# EC_1.b. Call the function from task EC_1.a
+
+
+# EC_2.a. Create a function that takes a list of letters as an argument and returns a string with all the letters in the list
+
+
+# EC_2.b. Call the function from task EC_2.a
+
+
