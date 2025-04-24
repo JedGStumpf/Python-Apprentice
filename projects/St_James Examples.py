dc95d9ddaa11ffe9f0faa1177589a8d7e0d5ff76
JedGStumpf <jedgstumpf@gmail.com>
2025-04-24 00:56:50 +0000

    4-23-2025

--- projects/St_James Examples.py
+++ projects/St_James Examples.py
@@ -6,21 +6,79 @@ board = [[0, 1, 2],
 
 
 # print(array[0][2])
-print(board[2][2])
+# print(board[2][2])
 
 
 def say_my_name(name):
     print(f"Hello {name}")
 
-say_my_name("St. James")
+# say_my_name("St. James")
 
 def print_my_list(my_list):
+
     print(my_list)
 
-print("Columns below")
-for column in board:
-    print_my_list(column)
+# print("Columns below")
+# for column in board:
+#     print_my_list(column)
         
-print(board[1][2])
+# print(board[1][2])
 
+
+row = None
+
+if row:
+    print("True")
+else:
+    print("False")
+
+
+
+
+
+my_dictionary = {}
+
+
+my_dictionary["name"] = "St. James"
+my_dictionary["age"] = 25
+
+
+# print(my_dictionary)
+
+del my_dictionary["age"]
+# print(my_dictionary)
+
+
+db = {}
+
+
+def add_to_db(db, key, value):
+    db[key] = value
+
+
+print(db)
+
+add_to_db(db, "name", "St. James")
+add_to_db(db, "age", "25")
+
+print(db)
+
+# del db["name"]
+print(db)
+
+
+
+
+def is_funny(definition):
+    for key, value in db.items():
+        # print(f"Key: {key}, Value: {value}")
+        if value in definition:
+            # return True
+            print(f"{value} is funny")
+
+        else:
+            print("Not funny")
+    
+is_funny("St. James")
+is_funny("this is wrong")
 
