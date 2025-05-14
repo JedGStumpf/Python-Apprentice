cdfdf1b8f8aa4fa45e33228e2f454c5cc5a9eadd
JedGStumpf <jedgstumpf@gmail.com>
2025-05-14 21:51:16 +0000

    Legacy Examples

--- lessons/01_Types_and_Logic/04_my_ages.py
+++ lessons/01_Types_and_Logic/04_my_ages.py
@@ -22,17 +22,39 @@ window.withdraw() # Hide the window
 
 # Here is how you ask the user's age in integer format. 
 # The first argument is the title of the window, the second is the message to the user.
-age = simpledialog.askinteger("Your Age", "How old are you?")  # ;
+# age = simpledialog.askinteger("Your Age", "How old are you?")  # ;
 
 # You could also ask the user for a float with simpledialog.askfloat()
 # ( but only do one of them, not both)
-age =  simpledialog.askfloat("Your Age", "How old are you?")  # ;
-
+# age =  simpledialog.askfloat("Your Age", "How old are you?")  # ;
+age = int(input("How old are you?"))  # ;  # This is a way to ask for an integer without using tkinter
 # Here is how you show the user a message window
 # The first parameter is the title of the window, 
 # the second is the message to show the user.
-messagebox.showinfo('What you are', "You are a baby.")
 
+message = ""
+# messagebox.showinfo('What you are', "You are a baby.")
+
+if age < 2:
+    message = "You are a baby."
+elif age < 5:
+    message = "You are a toddler."
+elif age < 13:
+    message = "You are a child."
+elif age < 20:
+    message = "You are a teen."
+elif age < 65:
+    message = "You are an adult."
+elif age >= 65:
+    message = "You are a senior."
+elif age == 12:
+    message = "You are pretty awesome!"
+else:
+    message = "Um, I don't know what you are."
+
+
+# messagebox.showinfo('What you are', message)
+print(message)  # Print the message to the console
 window.mainloop()  # Keeps the window open
 
 
--- lessons/01_Types_and_Logic/06_infuriating_calculator.py
+++ lessons/01_Types_and_Logic/06_infuriating_calculator.py
@@ -24,14 +24,27 @@ operation you can ask for a string with simpledialog.askstring().
 
 # Hide the window, hint: use the withdraw method
 
-# Ask the user for the first number   
+# Ask the user for the first number 
+first_num = int(input("First Number, Enter Your First Number"))
 
 # Ask the user for the second number
+second_num = int(input("Second Number, Enter Your Second Number"))
 
 # Ask the user for the math operation
+operation = input("Math Operation, Enter the math operation (add, subtract, multiply, divide)")
 
 # Use if-elif-else statements to provide the desired math operation on the numbers and display the result.
-
+if operation == "add":
+    print(first_num + second_num)
+elif operation == "subtract":
+    print(first_num - second_num)
+elif operation == "multiply":   
+    print(first_num * second_num)
+elif operation == "divide":
+    print(first_num / second_num)
+else:
+    # Display an error message
+    print("Unknown operation. Please enter add, subtract, multiply, or divide.")
 # If the user enters an unknown operation, display an error message. ( use messagebox.showerror()
 
 # Keep the window open
